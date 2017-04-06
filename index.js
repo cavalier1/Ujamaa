e83625369377e3f273fe6574289760faabcb88e7
Imani William <iawilli4@ncsu.edu>
2017-04-05 21:42:38 -0400

    exteneded initial message and added button for location from previous chatbot

--- index.js
+++ index.js
@@ -38,9 +38,11 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message) {
             text = event.message.text
-            if (text === ('hello' || 'Hello')) {
-              sendTextMessage(sender, "Hello, welcome to Ujamaa.")
-            }
+            //if (text === ('hello' || 'Hello')) {
+              sendTextMessage(sender, "Hello, welcome to Ujamaa. I'm here to help you find a black owned business with the products or services you're looking for. Please select a button")
+              askLocation()
+                //continue
+            //}
             else
               sendTextMessage(sender, "I'm sorry but I don't understand what you're saying")
         }
@@ -119,6 +121,35 @@ function sendGenericMessage(recipientId) {
       }
     }
   };  
-
+unction askLocation() {
+    messageData = {
+        "text":"What is your location?",
+        "quick_replies":[{
+            "content_type":"text",
+            "title":"Raleigh",
+            "payload":"Raleigh"
+        },
+        {
+            "content_type":"text",
+            "title":"Chapel Hill",
+            "payload":"Chapel Hill"
+        }]
+    }
+      request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
   callSendAPI(messageData);
 }
